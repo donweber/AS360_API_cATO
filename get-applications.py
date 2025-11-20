943362b9655aae2abcbcb1cc0417cc45f7f4fc6e
donweber <donald.weber@hcl.com>
2025-11-20 14:08:00 +0000

    add get-applications.py to gather apps

---
+++ get-applications.py
@@ -0,0 +1,87 @@
+import requests
+import argparse
+import json
+import urllib.parse
+import sys
+
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Fetch AppScan Issues by Status")
+    parser.add_argument("--ApiKey", required=True, help="API Key")
+    parser.add_argument("--ApiSecret", required=True, help="API Secret")
+    args = parser.parse_args()
+
+    # Base URL for AppScan 360
+    base_url = "https://ip-192-168-69-208.appscan.il/api/v4"
+
+    # Authenticate and get token
+    auth_body = {
+        "KeyId": args.ApiKey,
+        "KeySecret": args.ApiSecret
+    }
+
+    try:
+        auth_response = requests.post(
+            f"{base_url}/Account/APIKeyLogin",
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(auth_body),
+            verify=False  # Disable SSL verification
+        )
+        auth_response.raise_for_status()
+        token = auth_response.json().get("Token")
+        if not token:
+            print("Authentication token not received.")
+            sys.exit(2)
+        print(f"Auth successful. Token: {token}")
+    except requests.RequestException as e:
+        print(f"Failed to authenticate: {e}")
+        sys.exit(2)
+
+    # Prepare headers
+    headers = {"Authorization": f"Bearer {token}"}
+
+    # Build URL
+    apps_url = f"{base_url}/Apps?%24top=100&%24count=false"
+
+    print(f"Request URL: {apps_url}")
+
+    try:
+        apps_response = requests.get(apps_url, headers=headers, verify=False)
+        apps_response.raise_for_status()
+        app_data = apps_response.json()
+    except requests.RequestException as e:
+        print(f"Failed to retrieve applications: {e}")
+        sys.exit(3)
+
+    if not app_data or "Items" not in app_data or len(app_data["Items"]) == 0:
+        print(f"No Applications found")
+        sys.exit(0)
+
+    # Extract fields
+    filtered = []
+    for issue in app_data["Items"]:
+        filtered.append({
+            "applicationId": issue.get("ApplicationId"),
+            "applicationName": issue.get("Name"),
+            "RiskRating": issue.get("RiskRating"),
+            "AssetGroupName": issue.get("AssetGroupName"),
+            "AssetGroupId": issue.get("AssetGroupId"),
+            "Url": issue.get("Url"),
+            "Description": issue.get("Description"),
+            "BusinessUnit": issue.get("BusinessUnit"),
+            "BusinessUnitId": issue.get("BusinessUnitId"),
+            "BusinessOwner": issue.get("BusinessOwner"),
+            "CreatedBy": issue.get("CreatedBy"),
+            "TestingStatus": issue.get("TestingStatus"),
+            "TotalIssues": issue.get("TotalIssues")
+
+        })
+
+    # Output filtered issues as JSON
+    print(json.dumps(filtered, indent=4))
+
+    # Print summary
+    print(f"Summary: {len(filtered)} applications found")
+
+if __name__ == "__main__":
+    main()
--- lazyMan.py
+++ lazyMan.py
@@ -9,8 +9,11 @@ python get-FixGroups.py --Scope Application --ScanId 9d565b00-21c0-4d81-b428-9e2
 python get-FixGroups.py --Scope ScanExecution --ScanId dc242882-d2eb-4064-807a-8448405685b4 --ApiKey local_90e47a5b-9f48-fb1a-b59a-16ff0d03b76d --ApiSecret l9d2k4MpW5iAeabh5mNMc26AMc1YnlvUm9bpQW4myGA9 --Status Open
 
 python get-issue-details.py --IssueId a41b433e-f36b-1410-870f-00659a64aee6 --ApiKey local_90e47a5b-9f48-fb1a-b59a-16ff0d03b76d --ApiSecret l9d2k4MpW5iAeabh5mNMc26AMc1YnlvUm9bpQW4myGA9
-python get-issue-history.py --IssueId b01b433e-f36b-1410-870f-00659a64aee6 --ApiKey local_90e47a5b-9f48-fb1a-b59a-16ff0d03b76d --ApiSecret l9d2k4MpW5iAeabh5mNMc26AMc1YnlvUm9bpQW4myGA9 --Status Fixed
+python get-issue-history.py --IssueId cc83433e-f36b-1410-8ab3-003ec2e90a70 --ApiKey local_90e47a5b-9f48-fb1a-b59a-16ff0d03b76d --ApiSecret l9d2k4MpW5iAeabh5mNMc26AMc1YnlvUm9bpQW4myGA9 --Status Fixed --AfterTimeStamp "2025-11-03T15:00:58.8953259Z"
 				      cc83433e-f36b-1410-8ab3-003ec2e90a70
 
 					b01b433e-f36b-1410-870f-00659a64aee6
 a41b433e-f36b-1410-870f-00659a64aee6
+JyIDJzKcF2qjkMlQhsaLbXY-mTBqoRxJ
+
+python compare-datetimestamps.py --t1 "2025-09-06T08:57:07.0952628Z" --t2 "2025-11-14T18:31:24.1280083Z"
